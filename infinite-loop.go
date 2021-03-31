560bb86f9b268b9a693d6e5708215a706ce30506
i-shobha <shobha.rdg1@gmail.com>
2021-03-31 14:55:49 +0530

    Uploading the GO files

---
+++ infinite-loop.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+   for true  {
+       fmt.Printf("Greetings of the Day..! HAPPY UGADI...!\n");
+       fmt.Printf("This loop will run forever,press ctrl+C to end.\n");
+   }
+}
